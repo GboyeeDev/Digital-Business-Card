c878d6be0692656b62e8e52a8cfa93414699f512
Olugboyega <114001863+GboyeeDev@users.noreply.github.com>
2023-03-05 21:26:39 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About() {
         <div>
             <h1 className="about">About</h1>
             <p>
-                I am a backend developer with a particular interest in making things 
+                I am a backend developer with a particular interest in making  
                 simple and automating daily tasks. 
                 I try to keep up with security and best practices, and am always 
                 looking  for new things to learn.
