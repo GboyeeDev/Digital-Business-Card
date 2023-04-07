e5ca7ab757d0f136cd596ff4f216206b00fb2a53
Olugboyega <114001863+GboyeeDev@users.noreply.github.com>
2023-04-07 20:00:30 +0100

    Update Info.js

--- src/Info.js
+++ src/Info.js
@@ -8,7 +8,7 @@ function Info() {
         <div className="info">
             <img src={image} className="img" alt="description" />
             <h1 className="name">Olugboyega Ojelabi</h1>
-            <h2 className="title">Frontend Developer</h2>
+            <h2 className="title">Backend Developer</h2>
             <a className="website" href="https://olugboyega-ojelabi.netlify.app">olugboyega-ojelabi.netlify.app</a>
             <div className="button">
                 <a href="mailto:olugboyega.ojelabi@gmail.com" target="_blank" rel="noopener noreferrer">
